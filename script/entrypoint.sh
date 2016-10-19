30720e4826a89b54e70c0df0b0f7f272ced45756
Andrey P <prih@prihhost.com>
2016-10-20 00:13:28 +0300

    fix issue #1

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -10,8 +10,6 @@ echo $TZ > /etc/timezone && \
 echo "ssl_key=${SSL_KEY:=le-key.pem}, ssl_cert=${SSL_CERT:=le-crt.pem}"
 SSL_KEY=/etc/nginx/ssl/${SSL_KEY}
 SSL_CERT=/etc/nginx/ssl/${SSL_CERT}
-sed -i "s|SSL_KEY|${SSL_KEY}|g" /etc/nginx/conf.d/service.conf
-sed -i "s|SSL_CERT|${SSL_CERT}|g" /etc/nginx/conf.d/service.conf
 
 if [ ! -f /etc/nginx/ssl/dhparams.pem ]; then
     echo "make dhparams"
@@ -29,7 +27,11 @@ fi
     /le.sh
     rm -f /etc/nginx/conf.d/default.conf 2>/dev/null #remove default config, conflicting on 80
     mv -v /etc/nginx/conf.d.disabled /etc/nginx/conf.d #enable
+    
     cp -f /etc/nginx/service.conf /etc/nginx/conf.d/service.conf
+    sed -i "s|SSL_KEY|${SSL_KEY}|g" /etc/nginx/conf.d/service.conf
+    sed -i "s|SSL_CERT|${SSL_CERT}|g" /etc/nginx/conf.d/service.conf
+    
     echo "reload nginx with ssl"
     nginx -s reload
     sleep 60d
