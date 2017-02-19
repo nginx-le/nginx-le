e7e4225e114abcfc750ff57087f1e8a6f7cfbbd6
eugene <eugene@umputn.com>
2017-02-19 15:08:58 -0600

    coma separated list of domains for destination #5

--- script/le.sh
+++ script/le.sh
@@ -2,8 +2,9 @@
 
 if [ "$LETSENCRYPT" = "true" ]; then
     certbot certonly -t -n --agree-tos --renew-by-default --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d $LE_FQDN
-    cp -fv /etc/letsencrypt/live/$LE_FQDN/privkey.pem /etc/nginx/ssl/le-key.pem
-    cp -fv /etc/letsencrypt/live/$LE_FQDN/fullchain.pem /etc/nginx/ssl/le-crt.pem
+    FIRST_FQDN=$(echo "$LE_FQDN" | cut -d"," -f1)
+    cp -fv /etc/letsencrypt/live/$FIRST_FQDN/privkey.pem /etc/nginx/ssl/le-key.pem
+    cp -fv /etc/letsencrypt/live/$FIRST_FQDN/fullchain.pem /etc/nginx/ssl/le-crt.pem
 else
     echo "letsencrypt disabled"
 fi
