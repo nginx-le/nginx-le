da1614907092bfbaf3262d1d7c787a7028ba1d39
Dmitry Verkhoturov <paskal.07@gmail.com>
2018-11-24 01:36:16 +0300

    add certificate domain names check

--- script/le.sh
+++ script/le.sh
@@ -12,9 +12,21 @@ if [ "$LETSENCRYPT" != "true" ]; then
     return 1
 fi
 
-if [ -f ${target_cert} ] && openssl x509 -checkend  ${renew_before} -noout -in ${target_cert} ; then
-    echo "letsencrypt certificate still valid"
-    return 1
+# redirection to /dev/null to remove "Certificate will not expire" output
+if [ -f ${target_cert} ] && openssl x509 -checkend ${renew_before} -noout -in ${target_cert} > /dev/null ; then
+    # egrep to remove leading whitespaces
+    CERT_FQDNS=$(openssl x509 -in ${target_cert} -text -noout | egrep -o 'DNS.*')
+    # run and catch exit code separately because couldn't embed $@ into `if` line properly
+    set -- $(echo ${LE_FQDN} | tr ',' '\n'); for element in "$@"; do echo ${CERT_FQDNS} | grep -q $element ; done
+    CHECK_RESULT=$?
+    if [ ${CHECK_RESULT} -eq 0 ] ; then
+        echo "letsencrypt certificate ${target_cert} still valid"
+        return 1
+    else
+        echo "letsencrypt certificate ${target_cert} is present, but contains wrong domains"
+        echo "expected: ${LE_FQDN}"
+        echo "found:    ${CERT_FQDNS}"
+    fi
 fi
 
 echo "letsencrypt certificate will expire soon or missing, renewing..."
