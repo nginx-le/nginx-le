63ff2a759ac935a3322d96755a1e80808b37ca19
Dmitry Verkhoturov <paskal.07@gmail.com>
2020-10-17 15:52:17 +0200

    add streams configuration support

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -15,23 +15,32 @@ export LE_SSL_CHAIN_CERT=/etc/nginx/ssl/${SSL_CHAIN_CERT}
 
 #create destination folders
 mkdir -p /etc/nginx/conf.d
+mkdir -p /etc/nginx/stream.d
 mkdir -p /etc/nginx/ssl
 
-#collect services
+#collect services and streams
 SERVICES_FILES=$(find "/etc/nginx/" -type f -maxdepth 1 -name "service*.conf")
+STREAMS_FILES=$(find "/etc/nginx/" -type f -maxdepth 1 -name "stream*.conf")
 
-#copy service*.conf from /etc/nginx/ if they are mounted
+#copy service*.conf and stream*.conf from /etc/nginx/ if they are mounted
 if [ ${#SERVICES_FILES} -ne 0 ]; then
     cp -fv /etc/nginx/service*.conf /etc/nginx/conf.d/
+fi
+if [ ${#STREAMS_FILES} -ne 0 ]; then
+    cp -fv /etc/nginx/stream*.conf /etc/nginx/stream.d/
 fi
 
 #replace SSL_KEY, SSL_CERT and SSL_CHAIN_CERT by actual keys
 sed -i "s|SSL_KEY|${LE_SSL_KEY}|g" /etc/nginx/conf.d/*.conf 2>/dev/null
+sed -i "s|SSL_KEY|${LE_SSL_KEY}|g" /etc/nginx/stream.d/*.conf 2>/dev/null
 sed -i "s|SSL_CERT|${LE_SSL_CERT}|g" /etc/nginx/conf.d/*.conf 2>/dev/null
+sed -i "s|SSL_CERT|${LE_SSL_CERT}|g" /etc/nginx/stream.d/*.conf 2>/dev/null
 sed -i "s|SSL_CHAIN_CERT|${LE_SSL_CHAIN_CERT}|g" /etc/nginx/conf.d/*.conf 2>/dev/null
+sed -i "s|SSL_CHAIN_CERT|${LE_SSL_CHAIN_CERT}|g" /etc/nginx/stream.d/*.conf 2>/dev/null
 
 #replace LE_FQDN
 sed -i "s|LE_FQDN|${LE_FQDN}|g" /etc/nginx/conf.d/*.conf 2>/dev/null
+sed -i "s|LE_FQDN|${LE_FQDN}|g" /etc/nginx/stream.d/*.conf 2>/dev/null
 
 #generate dhparams.pem
 if [ ! -f /etc/nginx/ssl/dhparams.pem ]; then
@@ -43,6 +52,7 @@ fi
 
 #disable configuration and let it run without SSL
 mv -v /etc/nginx/conf.d /etc/nginx/conf.d.disabled
+mv -v /etc/nginx/stream.d /etc/nginx/stream.d.disabled
 
 (
  sleep 5 #give nginx time to start
@@ -54,6 +64,7 @@ mv -v /etc/nginx/conf.d /etc/nginx/conf.d.disabled
     rm -f /etc/nginx/conf.d/default.conf 2>/dev/null
     #on the first run enable config back
     mv -v /etc/nginx/conf.d.disabled /etc/nginx/conf.d 2>/dev/null
+    mv -v /etc/nginx/stream.d.disabled /etc/nginx/stream.d 2>/dev/null
     echo "reload nginx with ssl"
     nginx -s reload
     sleep 10d
